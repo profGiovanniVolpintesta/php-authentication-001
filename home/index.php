937844b6416d5765dd7b27135bb3ac679aa32ca6
Giovanni Volpintesta <149997695+profGiovanniVolpintesta@users.noreply.github.com>
2026-02-04 09:58:09 +0100

    Bottone "Login"
    Bottone "Login" homepage non loggata

---
+++ home/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    Homepage non loggata<br>
+    <button type="button" onclick="window.location.href='../login'">Login</button>
+</body>
+</html>
