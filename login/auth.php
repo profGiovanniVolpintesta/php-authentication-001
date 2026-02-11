476623383342b65501acc5303201b1b1fdd4522b
Giovanni Volpintesta <149997695+profGiovanniVolpintesta@users.noreply.github.com>
2026-02-11 10:06:32 +0100

    Bugfix
    Bugfix

--- login/auth.php
+++ login/auth.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST))
         $dbname = "volpintesta_autenticazione_php";
 
         try {
-            $conn = new PDO("mysql:host=$servername;,dbname=$dbname", $dbUsername, $dbPassword);
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbUsername, $dbPassword);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
