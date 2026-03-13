2f7446c75bb634a39f6f40127d399e419049c589
Giovanni Volpintesta <giovanni.volpintesta@itisriva.edu.it>
2026-03-13 12:52:21 +0100

    Categorie di permessi (dati)
    Categorie di permessi (dati)

--- Users_table.sql
+++ Users_table.sql
@@ -1,39 +1,61 @@
+DROP table user_rights;
+DROP table rights;
 DROP table users;
+DROP table users_types;
+
+CREATE TABLE users_types (
+	name varchar(30) primary key
+);
 
 CREATE TABLE users (
 	id integer auto_increment primary key,
     username varchar(100) unique not null,
-    password varchar(100) not null
+    password varchar(100) not null,
+    userType varchar(30) not null,
+    FOREIGN KEY (userType) REFERENCES users_types(name)
 );
 
 CREATE TABLE rights (
-    id integer auto_increment primary key,
-    name varchar(30) unique not null
+    name varchar(30) primary key
 );
 
 CREATE TABLE user_rights (
-
-	userId integer,
-    rightId integer,
-    PRIMARY KEY (userId, rightId),
-    FOREIGN KEY (userId) REFERENCES users(id),
-    FOREIGN KEY (rightId) REFERENCES rights(id)
+	userType varchar(30),
+    rights varchar(30),
+    PRIMARY KEY (userType, rights),
+    FOREIGN KEY (userType) REFERENCES users_types(name),
+    FOREIGN KEY (rights) REFERENCES rights(name)
 );
 
-INSERT INTO users(username, password)
-VALUES ("giovanni.volpintesta", "gv"),
-("giuseppe.grasso", "gg"),
-("alice", "aa");
+INSERT INTO users_types(name)
+VALUES ("OWNER"),
+("COACH"),
+("USER");
+
+INSERT INTO users(username, password, userType)
+VALUES ("giovanni.volpintesta", "gv", "OWNER"),
+("giuseppe.grasso", "gg", "COACH"),
+("alice", "aa", "USER");
 
 INSERT INTO rights(name)
-VALUES ("USER_CREATE"),
+VALUES ("USER_VISUALIZE"),
+("USER_CREATE"),
+("COACH_VISUALIZE"),
 ("COACH_CREATE");
 
-INSERT INTO user_rights(userId, rightId)
-VALUES (1, 1),
-(1, 2),
-(2, 2);
+INSERT INTO user_rights(userType, rights)
+VALUES ("OWNER", "USER_VISUALIZE"),
+("OWNER", "USER_CREATE"),
+("OWNER", "COACH_VISUALIZE"),
+("OWNER", "COACH_CREATE"),
+("COACH", "USER_VISUALIZE"),
+("COACH", "USER_CREATE"),
+("COACH", "COACH_VISUALIZE"),
+("USER", "COACH_VISUALIZE");
 
+select * from users_types;
 select * from users;
+select * from rights;
+select * from user_rights order by user_rights.userType, user_rights.rights;
 
 SELECT username FROM users WHERE username = 'giovanni.volpintesta' AND password = 'gv';
